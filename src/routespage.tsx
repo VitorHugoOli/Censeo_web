20448eada5ab692fc6fe609834678db7abbbef66
vitor <vho2009@hotmail.com>
2021-11-25 12:55:17 -0300

    pag privacidade

--- src/routespage.tsx
+++ src/routespage.tsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from "./pages/home";
+import Privacidade from "./pages/privacidade";
 
 
 
@@ -9,6 +10,7 @@ function RoutePage() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/privacidade" element={<Privacidade/>}/>
             </Routes>
         </BrowserRouter>
     )
